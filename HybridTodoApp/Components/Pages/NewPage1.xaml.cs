72c04744e5baae27a5c3863d349b5cae50764be8
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-08-27 16:23:50 +0700

    menambahkan integrasi dengan blazor component dan maui native

---
+++ HybridTodoApp/Components/Pages/NewPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace HybridTodoApp.Components.Pages;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
--- HybridTodoApp/MainPage.xaml.cs
+++ HybridTodoApp/MainPage.xaml.cs
@@ -1,15 +1,12 @@
 ﻿namespace HybridTodoApp
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
-        {
-            await DisplayAlert("Native UI", "This is from Native MAUI Button", "OK");
-        }
+
     }
 }
