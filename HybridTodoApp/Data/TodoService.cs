7c9e77a320b0a09138950726333cce76e5b01c20
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-08-27 11:39:09 +0700

    menambahkan json save

---
+++ HybridTodoApp/Data/TodoService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HybridTodoApp.Data
+{
+    public class TodoService
+    {
+        string file = string.Empty;
+
+        public TodoService()
+        {
+            file = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "todos.json");
+        }
+
+        public void SaveItems(IEnumerable<TodoItem> items)
+        {
+            var json = JsonSerializer.Serialize(items);
+            File.WriteAllText(file, json);
+        }
+    }
+}
